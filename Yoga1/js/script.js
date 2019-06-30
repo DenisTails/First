8ea4c351022663c68424286463f01bece955c9fe
Denis <Denis-saratov2001@mail.ru>
2019-06-30 23:28:47 +0400

    Request for web. I need a rest after that ZzzZzzZzz

--- Yoga1/js/script.js
+++ Yoga1/js/script.js
@@ -112,4 +112,82 @@ overlay.style.display = 'block';
 document.body.style.overflow = 'hidden';
 });
 }
+
+//Form
+
+let massege = {
+    loading: 'Идёт отправка...',
+    complete: 'Спасибо что подождали, оправка завершена!',
+    failure: 'Ой... Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input');
+
+let statusMassage = document.createElement('div');
+statusMassage.classList.add('status');
+
+let form2 = document.getElementById('form');
+
+form2.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    form2.appendChild(statusMassage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST','server.php');
+    request.setRequestHeader('Content-Type','application/json; charset =utf-8');
+
+    let obj={};
+
+    let formData = new FormData();
+
+    formData.forEach((value, key)=>{
+        obj[key] = value;
+    });
+
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', ()=>{
+        if(request.readyState < 4){
+            statusMassage.innerHTML = massege.loading;
+        } else if (request.readyState === 4 && request.status == 200){
+            statusMassage.innerHTML =massege.complete;
+        } else{
+            statusMassage.innerHTML =massege.failure;
+        }
+    });
+});
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    form.appendChild(statusMassage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST','server.php');
+    request.setRequestHeader('Content-Type','application/json; charset =utf-8');
+
+    let obj={};
+
+    let formData = new FormData();
+
+    formData.forEach((value, key)=>{
+        obj[key] = value;
+    });
+
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', ()=>{
+        if(request.readyState < 4){
+            statusMassage.innerHTML = massege.loading;
+        } else if (request.readyState === 4 && request.status == 200){
+            statusMassage.innerHTML =massege.complete;
+        } else{
+            statusMassage.innerHTML =massege.failure;
+        }
+    });
+});
 });
